89aead3f528065af700e6e608698dc34475fe2d8
Matt Brooks <matt@msbrooks.com>
2022-11-27 19:45:09 -0600

    Adding decay after note played

--- play_sine.py
+++ play_sine.py
@@ -7,56 +7,70 @@ import sounddevice as sd
 
 device = 1
 start_idx = 0
-amplitude = 0.2
-frequency = 440
+frequency = 0
+amplitude = 0
 
-filedescriptors = termios. tcgetattr(sys. stdin)
-tty. setcbreak(sys. stdin)
+default_amplitude = 0.5
+
+decay = 0.97
+
+filedescriptors = termios. tcgetattr(sys.stdin)
+tty.setcbreak(sys.stdin)
 
 samplerate = sd.query_devices(device, 'output')['default_samplerate']
 
 def callback(outdata, frames, time, status):
 	if status:
 		print(status, file=sys.stderr)
-	global start_idx
+	global start_idx, amplitude
+
+	# decay amplitude
+	amplitude = amplitude * decay
+
 	t = (start_idx + np.arange(frames)) / samplerate
 	t = t.reshape(-1, 1)
-	outdata[:] = amplitude * np.sin(2 * np.pi * frequency * t)
+
+	outdata[:] = amplitude * np.cos(3 * 30 * frequency * t)
 	start_idx += frames
 
 with sd.OutputStream(device=device, channels=1, callback=callback,
 						samplerate=samplerate):
 	print('#' * 80)
-	print('press Return to quit')
+	print('press Enter to quit')
 	print('#' * 80)
 
+	print('Enter a single character: ')
 
 	while True:
-		print('Enter a single character: ')
 		user_input = sys.stdin.read(1)[0]
+
+		# if enter pressed exit
+		if ord(user_input) == 10:
+			break
 
 		if len(user_input) == 1:
 			match user_input:
 				case 'a':
-					frequency = 440
+					amplitude = default_amplitude
+					frequency = 16.35
 				case 's':
-					frequency = 500
+					amplitude = default_amplitude
+					frequency = 18.35
 				case 'd':
-					frequency = 560
+					amplitude = default_amplitude
+					frequency = 20.60
 				case 'f':
-					frequency = 600
+					amplitude = default_amplitude
+					frequency = 21.83
 				case 'g':
-					frequency = 680
+					amplitude = default_amplitude
+					frequency = 24.50
 				case 'h':
-					frequency = 760
+					amplitude = default_amplitude
+					frequency = 27.50
 				case 'j':
-					frequency = 840
+					amplitude = default_amplitude
+					frequency = 30.87
 				case 'k':
-					frequency = 880
-
-		elif user_input == '':
-			break
-
-		else:
-			print('Enter a single character to continue.')
-			continue
+					amplitude = default_amplitude
+					frequency = 16.35 * 2
